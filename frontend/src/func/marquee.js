bb484bad2377eebd2b88149d1abe3d3e45901482
gusfing <gusfing@users.noreply.github.com>
2025-05-31 23:33:04 +0600

    Modify marquee code

--- frontend/src/func/marquee.js
+++ frontend/src/func/marquee.js
@@ -1,69 +1,91 @@
-const gsap = require("gsap");
+import { gsap } from "gsap";
 
-function setupMarqueeAnimation() {
-    const marqueeItems = gsap.utils.toArray(".marquee h1");
-    if (marqueeItems > 0) {
-        const tl = horizontalLoop(marqueeItems, {
-            repeat: -1,
-            paddingRight: 30,
-        });
-    }
-}
-
+// Horizontal loop helper function
 function horizontalLoop(items, config) {
     items = gsap.utils.toArray(items);
     config = config || {};
-    let tl = gsap.timeline({
+    const tl = gsap.timeline({
         repeat: config.repeat,
         defaults: { ease: "none" },
+        paused: config.paused
     });
-    let length = items.length;
-    let startX = items[0].offsetLeft;
-    let widths = [];
-    let xPercents = [];
-    let pixelsPerSecond = (config.speed || 1) * 100;
-    let totalWidth, cirX, distanceToStart, distanceToLoop, item, i;
 
+    const length = items.length;
+    const startX = items[0].offsetLeft;
+    const widths = [];
+    const xPercents = [];
+    const pixelsPerSecond = (config.speed || 1) * 100;
+    let totalWidth;
+
+    // Get initial positions
     gsap.set(items, {
-        xPercents: (i, el) => {
-            let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
-            xPercents[i] =
-                (parseFloat(gsap.getProperty(el, "x", "px")) / w) * 100 + gsap.getProperty(el, "xPercent");
+        xPercent: (i, el) => {
+            const w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
+            xPercents[i] = (parseFloat(gsap.getProperty(el, "x", "px")) / w) * 100 +
+                gsap.getProperty(el, "xPercent");
             return xPercents[i];
         }
     });
 
     gsap.set(items, { x: 0 });
-    totalWidth =
-        items[length - 1].offsetLeft + (xPercents[length - 1] / 100) * widths[length - 1] - startX + items[length - 1].offsetWidth * gsap.getProperty(items[length - 1], "scaleX") + (parseFloat(config.paddingRight) || 0);
 
-    for (i = 0; i < length; i++) {
-        item = items[i];
-        curX = (xPercents[i] / 100) * widths[i];
-        distanceToStart = item.offsetLeft + curX - startX;
+    // Calculate total width
+    totalWidth = items[length - 1].offsetLeft +
+        (xPercents[length - 1] / 100) * widths[length - 1] -
+        startX +
+        items[length - 1].offsetWidth *
+        gsap.getProperty(items[length - 1], "scaleX") +
+        (parseFloat(config.paddingRight) || 0);
 
-        distanceToLoop =
-            distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+    // Create animation
+    for (let i = 0; i < length; i++) {
+        const item = items[i];
+        const curX = (xPercents[i] / 100) * widths[i];
+        const distanceToStart = item.offsetLeft + curX - startX;
+        const distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+
         tl.to(
             item,
             {
                 xPercent: ((curX - distanceToLoop) / widths[i]) * 100,
-                duration: distanceToLoop / pixelsPerSecond,
+                duration: distanceToLoop / pixelsPerSecond
             },
             0
-
         ).fromTo(
             item,
             {
                 xPercent: ((curX - distanceToLoop + totalWidth) / widths[i]) * 100
-            }, {
-            xPercent: xPercents[i],
-            duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
-            immediateRender: false,
-        },
+            },
+            {
+                xPercent: xPercents[i],
+                duration: (totalWidth - distanceToLoop) / pixelsPerSecond,
+                immediateRender: false
+            },
             distanceToLoop / pixelsPerSecond
         );
     }
-    tl.progress(1, true).progress(0, true);
+
     return tl;
+}
+
+// Marquee setup function
+export function setupMarqueeAnimation() {
+    const marqueeItems = gsap.utils.toArray(".marquee h1");
+
+    if (marqueeItems.length > 0) {
+        return horizontalLoop(marqueeItems, {
+            repeat: -1,
+            paddingRight: 30,
+            speed: 1
+        });
+    }
+    return null;
+}
+
+// React hook version
+export function useMarqueeAnimation() {
+    useEffect(() => {
+        const animation = setupMarqueeAnimation();
+        return () => animation?.kill(); // Cleanup
+    }, []);
 }
