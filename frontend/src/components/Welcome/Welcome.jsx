af7caef5e06375511d6ae05ec1777620a6b535c6
gusfing <gusfing@users.noreply.github.com>
2025-10-11 18:40:09 +0600

    Fix welcome section height

--- frontend/src/components/Welcome/Welcome.jsx
+++ frontend/src/components/Welcome/Welcome.jsx
@@ -34,9 +34,9 @@ const Welcome = () => {
     });
 
     return (
-        <div className='welcome-section w-full h-dvh text-[#2A2725]  md:px-7 px-6'>
+        <div className='welcome-section w-full h-[120vh] text-[#2A2725]  md:px-7 px-6 border border-amber-500'>
             <div className='flex flex-col gap-2 tracking-[-4] leading-2'>
-                <div className="w-full md:w-[86%] md:text-[64px] text-[34px] welcome-line md:py-20">
+                <div className="w-full md:w-[86%] md:text-[64px] text-[34px] welcome-line md:pt-20">
                     <div className="w-full welcome-text flex flex-col justify-center items-start">
                         {welcomeLines.map((text, index) => (
                             <span key={index} className="relative block text-darkBrown md:tracking-[-0.010em] tracking-[0.015em]">
@@ -47,7 +47,7 @@ const Welcome = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex md:flex-row flex-col justify-between items-center md:p-4 md:mt-0 mt-10">
+            <div className="flex md:flex-row flex-col justify-between items-center md:p-4 md:mt-20 mt-10">
                 <div className="flex flex-row justify-center items-center gap-1">
                     <img src={w1} alt="welcome image" className="md:rounded-[8rem] rounded-[9rem] md:w-56 w-44" />
                     <img src={w2} alt="welcome image" className="md:rounded-[8rem] rounded-[9rem] md:w-56 w-44" />
